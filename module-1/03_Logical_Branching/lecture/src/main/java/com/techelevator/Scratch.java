bef072701db76770585473e35276a92db8786907
Test Student <christopher@techelevator.com>
2023-01-24 18:07:19 -0500

    adding code for logical branching

---
+++ module-1/03_Logical_Branching/lecture/src/main/java/com/techelevator/Scratch.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class Scratch {
+
+    public static void main(String[] args) {
+
+        int balance = 1000;
+        int withdraw = 20;
+
+
+
+        System.out.println("Our balance is: $" + balance);
+    }
+}
