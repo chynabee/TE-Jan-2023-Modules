73058764570664e19ee1bd3ecb341854a386fef1
Test Student <christopher@techelevator.com>
2023-02-02 08:57:06 -0500

    lecture for classes

---
+++ module-1/09_Classes_and_Encapsulation/lecture/src/main/java/com/techelevator/Main.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create a new class, initializing the first name
+        // and last name through the constructor
+        Person christopher = new Person("Christopher", "Guarnera");
+        christopher.introduceSelf();
+
+
+    }
+
+}
