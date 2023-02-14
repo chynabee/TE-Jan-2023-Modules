0c967a697c2f17eeae2bf0fd0c39f2b469565c02
Chyna Broyles <chynabroyles@gmail.com>
2023-02-13 21:17:01 -0500

    Submitting File IO exercise

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordSearch {
@@ -14,6 +16,60 @@ public class WordSearch {
 
 	public void run() {
 		/* Your code goes here */
+
+Scanner consoleInput = new Scanner(System.in);
+
+		System.out.println("Please enter the file:");
+		String fileName = consoleInput.nextLine();
+
+		System.out.println("What is the search word you are looking for");
+		String wordSearched = consoleInput.nextLine();
+
+		System.out.println("Should the search be case sensitive? (Y/N)" );
+		String isCaseSensitive = consoleInput.nextLine();
+
+		//Step 1: Create file object in Java
+		File file = new File(fileName);
+
+		//Step 2: Open a try with resources with a scanner
+		try(Scanner fileScanner = new Scanner(file)) {
+		int lineNumber = 0;
+
+
+
+			//Step 3: read from the file as long as there is a new line to read
+			while(fileScanner.hasNextLine()) {
+
+				//Step 4: read the line from the file and save it as a string
+				String line = fileScanner.nextLine();
+				lineNumber++;
+
+				if(isCaseSensitive.equalsIgnoreCase("N")){
+					String lowercaseLine = line.toLowerCase();
+					String lowercaseWordSearched = wordSearched.toLowerCase();
+
+					if(lowercaseLine.contains(lowercaseWordSearched)) {
+						System.out.println(lineNumber + ") " + line);
+					}
+
+				}
+
+
+
+				//Step 5: implement an if or whatever business logic- we want to print
+
+				else if(line.contains(wordSearched)) {
+					System.out.println(lineNumber + ") " + line);
+				}
+
+			}
+
+
+
+
+		} catch (FileNotFoundException ex) {
+		System.out.println("File was not found");
+		}
 
 
 	}
