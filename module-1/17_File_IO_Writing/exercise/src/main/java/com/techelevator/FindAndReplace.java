e65abea1e204345c0727737562e40547700ed25a
Chyna Broyles <chynabroyles@gmail.com>
2023-02-14 16:34:25 -0500

    Submitting File IO Writing Exercise

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -14,6 +17,51 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+
+        int counter = 0;
+
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourceFileName = userInput.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationFileName = userInput.nextLine();
+
+        File sourceFile = new File(sourceFileName);
+        File destinationFile = new File(destinationFileName);
+
+        try(Scanner fileScanner = new Scanner (sourceFile);
+            PrintWriter writer = new PrintWriter(destinationFile)) {
+
+            while(fileScanner.hasNextLine()) {
+
+                String line = fileScanner.nextLine();
+                counter++;
+
+
+                   String newline = line.replaceAll(searchWord, replacementWord);
+
+
+                writer.println(newline);
+
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+
+
+
+
+
+
+
+
+
 
 
     }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FizzWriter {
@@ -16,6 +18,31 @@ public class FizzWriter {
 		/* Your code goes here */
 
 
-	}
+		System.out.println("Please enter the file to save results:");
+		String resultsFizzBuzzFileName = userInput.nextLine();
 
+		File fizzFile = new File(resultsFizzBuzzFileName);
+
+		try (PrintWriter writer = new PrintWriter(fizzFile)) {
+			int number = 0;
+			for (int i = 1; i <= 300; i++) {
+				number++;
+
+				if (number % 3 == 0 && number % 5 == 0) {
+					writer.println("FizzBuzz");
+				} else if (number % 3 == 0) {
+					writer.println("Fizz");
+				} else if (number % 5 == 0) {
+					writer.println("Buzz");
+				} else {
+					writer.println(number);
+				}
+			}
+
+
+		} catch (Exception ex) {
+			System.out.println("Something went wrong");
+
+		}
+	}
 }
