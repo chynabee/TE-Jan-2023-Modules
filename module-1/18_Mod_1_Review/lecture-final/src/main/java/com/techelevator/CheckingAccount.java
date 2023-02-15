ba4cb8ebba6841652a5db9170e50e9d0e4e42415
Test Student <christopher@techelevator.com>
2023-02-15 12:08:01 -0500

    review

---
+++ module-1/18_Mod_1_Review/lecture-final/src/main/java/com/techelevator/CheckingAccount.java
@@ -0,0 +1,37 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+//If we want to inherit from another class, we say extends
+// and then that other class name
+public class CheckingAccount extends BankAccount{
+
+    //constants
+    private final BigDecimal OVERDRAFT_FEE = new BigDecimal("20.00");
+
+    //instance variables
+    private boolean requestedDebitCard;
+
+    //constructors
+    public CheckingAccount(String name) {
+        //any time that we want to reference our parent
+        //we can use the keyword super
+        super(name);
+    }
+
+    public CheckingAccount(String name, BigDecimal balance){
+        super(name, balance);
+    }
+
+    //getters and setters
+
+    //NOTE: when a variable is a boolean, we use "is" instead of "get"
+    //when naming the getter
+    public boolean isRequestedDebitCard() {
+        return requestedDebitCard;
+    }
+
+    public void setRequestedDebitCard(boolean requestedDebitCard) {
+        this.requestedDebitCard = requestedDebitCard;
+    }
+}
