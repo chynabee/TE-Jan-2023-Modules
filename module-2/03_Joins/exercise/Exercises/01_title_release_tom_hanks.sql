f61af9e64c3cb3e6dbb092a3b2b3adae2037a33e
Test Student <christopher@techelevator.com>
2023-02-24 11:51:25 -0500

    lecture and hw

--- module-2/03_Joins/exercise/Exercises/01_title_release_tom_hanks.sql
+++ module-2/03_Joins/exercise/Exercises/01_title_release_tom_hanks.sql
@@ -2,3 +2,11 @@
 -- Order the results by release date, newest to oldest.
 -- (47 rows)
 
+SELECT title, release_date
+FROM movie
+JOIN movie_actor
+	ON movie.movie_id = movie_actor.movie_id
+JOIN person
+	ON movie_actor.actor_id = person.person_id
+WHERE person_name = 'Tom Hanks'
+ORDER BY release_date DESC;
