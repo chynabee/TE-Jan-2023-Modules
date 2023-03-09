46304859c6afadc4c9f5a26ebaf89ef3593f3580
Test Student <christopher@techelevator.com>
2023-03-09 11:39:13 -0500

    hw and lecture

---
+++ module-2/14_Server_Side_APIs_Part_2/lecture-final/server/src/main/java/com/techelevator/reservations/model/Reservation.java
@@ -0,0 +1,91 @@
+package com.techelevator.reservations.model;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+public class Reservation {
+
+    private int id;
+
+    @Min(value = 1, message ="Please select the hotel you are staying at")
+    private int hotelId;
+
+    @NotBlank(message = "Please provide a name for your reservation.")
+    private String fullName;
+
+    @NotBlank(message = "Please provide a checkin date.")
+    private String checkinDate;
+
+    @NotBlank(message = "Please provide a checkout date")
+    private String checkoutDate;
+
+    @Min( value = 1, message = "Please provide the number of guests, minimum 1")
+    @Max( value = 5, message = "The max number of guests per room is 5")
+    private int guests;
+
+    public Reservation(int id, int hotelId, String fullName, String checkinDate, String checkoutDate, int guests) {
+        this.id = id;
+        this.hotelId = hotelId;
+        this.fullName = fullName;
+        this.checkinDate = checkinDate;
+        this.checkoutDate = checkoutDate;
+        this.guests = guests;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(String checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public String getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(String checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public int getGuests() {
+        return guests;
+    }
+
+    public void setGuests(int guests) {
+        this.guests = guests;
+    }
+
+    @Override
+    public String toString() {
+        return "\n--------------------------------------------" + "\n Reservation Details"
+                + "\n--------------------------------------------" + "\n Id: " + id + "\n Hotel Id: " + hotelId
+                + "\n Full Name: " + fullName + "\n Checkin Date: " + checkinDate + "\n Checkout Date: " + checkoutDate
+                + "\n Guests: " + guests;
+    }
+}
