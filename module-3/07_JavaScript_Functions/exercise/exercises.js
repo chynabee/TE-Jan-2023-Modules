dc0fc269c1ee9e42eb944bb15977564dfb12b19d
Chyna Broyles <chynabroyles@gmail.com>
2023-03-28 15:38:30 -0400

    JS Functions Exercise

--- module-3/07_JavaScript_Functions/exercise/exercises.js
+++ module-3/07_JavaScript_Functions/exercise/exercises.js
@@ -20,6 +20,22 @@
  * @returns {boolean} true if they are admitted
  */
 
+function isAdmitted(gpa, satScore = 0, recommendation = false) {
+
+    if(gpa > 4) {
+        return true;
+    } else if (satScore > 1300) {
+        return true;
+    } else if (gpa > 3 && recommendation) {
+        return true;
+    } else if(satScore > 1200 && recommendation){
+        return true;
+    }  else {
+        return false;
+    }
+}
+
+
 /**
  * Write a function called useParameterToFilterArray that accepts a filter function
  * as a parameter. Use this function to filter unfilteredArray and return the result.
@@ -27,70 +43,109 @@
  * @param {function} filterFunction the function to filter with
  * @returns {number[]} the filtered array
  */
-let unfilteredArray = [1, 2, 3, 4, 5, 6];
-
-/**
- * Write a function called makeNumber that takes two strings
- * of digits, concatenates them together, and returns
- * the value as a number.
- *
- * So if two strings are passed in "42293" and "443", then this function
- * returns the number 42293443.
- *
- * @param {string} first the first string of digits to concatenate
- * @param {string} [second=''] the second string of digits to concatenate
- * @returns {number} the resultant number
- */
-
-/**
- * Write a function called addAll that takes an unknown number of parameters
- * and adds all of them together. Return the sum.
- *
- * @param {...number} num a series of numbers to add together
- * @returns {number} the sum of all the parameters (or arguments)
- */
-
-/*
- * Write and document a function called makeHappy that takes
- * an array and prepends 'Happy ' to the beginning of all the
- * words and returns them as a new array. Use the `map` function.
- */
-
-/*
- * Write and document a function called getFullAddressesOfProperties
- * that takes an array of JavaScript objects. Each object contains the
- * following keys:
- *     * streetNumber
- *     * streetName
- *     * streetType
- *     * city
- *     * state
- *     * zip
- *
- * getFullAddressesOfProperties returns an array of strings. 
- * Each string is a mailing address generated from the data of a single JavaScript object. 
- * 
- * Each mailing address should have the following format:
- *    
- *  streetNumber streetName streetType city state zip
- *
- * Use `map` and an anonymous function.
- */
-
-/** 
- * Write and document a function called findLargest that uses `forEach`
- * to find the largest element in an array.
- * The function must work for strings and numbers.
- * 
- * For strings, "largest" means the word coming last in lexographical order.
- * Lexographic is similar to alphabetical order except that 
- * capital letters come before lowercase letters: 
- * 
- * "cat" < "dog" but "Dog" < "cat"
- *
- * @param {number[]|string[]} searchArray the array to search
- * @returns {number|string} the number or string that is largest
- **/
+ let unfilteredArray = [1, 2, 3, 4, 5, 6];
+ function useParameterToFilterArray(filterFunction){
+     return unfilteredArray.filter(filterFunction);
+ }
+ 
+ /**
+  * Write a function called makeNumber that takes two strings
+  * of digits, concatenates them together, and returns
+  * the value as a number.
+  *
+  * So if two strings are passed in "42293" and "443", then this function
+  * returns the number 42293443.
+  *
+  * @param {string} first the first string of digits to concatenate
+  * @param {string} [second=''] the second string of digits to concatenate
+  * @returns {number} the resultant number
+  */
+  function makeNumber(first, second){
+      return parseInt (first + second);
+  }
+ /**
+  * Write a function called addAll that takes an unknown number of parameters
+  * and adds all of them together. Return the sum.
+  *
+  * @param {...number} num a series of numbers to add together
+  * @returns {number} the sum of all the parameters (or arguments)
+  */
+     function addAll(){
+         const myArray = Array.from(arguments);
+         const total = myArray.reduce(
+             (sum, currentVal) => {
+             return sum + currentVal;
+ 
+             }, 0
+ 
+         ) 
+     return total
+ 
+ 
+     }
+ /*
+  * Write and document a function called makeHappy that takes
+  * an array and prepends 'Happy ' to the beginning of all the
+  * words and returns them as a new array. Use the `map` function.
+  */
+ function makeHappy(searchArray) {
+     let addHappy = searchArray.map((word) => {
+         return "Happy " + word;
+     });
+     return addHappy;
+ }
+ 
+ /*
+  * Write and document a function called getFullAddressesOfProperties
+  * that takes an array of JavaScript objects. Each object contains the
+  * following keys:
+  *     * streetNumber
+  *     * streetName
+  *     * streetType
+  *     * city
+  *     * state
+  *     * zip
+  *
+  * getFullAddressesOfProperties returns an array of strings. 
+  * Each string is a mailing address generated from the data of a single JavaScript object. 
+  * 
+  * Each mailing address should have the following format:
+  *    
+  *  streetNumber streetName streetType city state zip
+  *
+  * Use `map` and an anonymous function.
+  */
+ function getFullAddressesOfProperties(addresses){
+    return addresses.map((address)=>{
+    return (address.streetNumber)+ ' ' +(address.streetName)+ ' ' +(address.streetType)+ ' ' + (address.city)+ ' ' +(address.state)+ ' ' +(address.zip) 
+ })
+ }
+ /** 
+  * Write and document a function called findLargest that uses `forEach`
+  * to find the largest element in an array.
+  * The function must work for strings and numbers.
+  * 
+  * 
+  * For strings, "largest" means the word coming last in lexographical order.
+  * Lexographic is similar to alphabetical order except that 
+  * capital letters come before lowercase letters: 
+  * 
+  * "cat" < "dog" but "Dog" < "cat"
+  *
+  * @param {number[]|string[]} searchArray the array to search
+  * @returns {number|string} the number or string that is largest
+  **/
+  function findLargest(topGear){
+      return topGear.reduce(
+      (largest, currentVal) => {
+         if (currentVal < largest){
+         return largest;
+         } else   return currentVal 
+      }   
+      )
+ 
+  }
+ 
 
 
 /*
@@ -110,3 +165,5 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  *
  * Read the tests to verify you have the correct behavior.
  */
+   
+
