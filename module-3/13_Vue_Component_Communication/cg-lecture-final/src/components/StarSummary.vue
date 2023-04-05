a967ad92fd5d0f3d03c0b7580667532ffff1dea7
Test Student <christopher@techelevator.com>
2023-04-05 11:57:49 -0400

    lecture and hw

---
+++ module-3/13_Vue_Component_Communication/cg-lecture-final/src/components/StarSummary.vue
@@ -0,0 +1,22 @@
+<template>
+
+
+    <div class="well">
+        
+        <span class="amount">{{ numOfReviews }}</span>
+        {{rating}} Star Review{{ numOfReviews === 1 ? '' : 's' }}
+    </div>
+
+ 
+</template>
+
+<script>
+export default {
+    name: "star-summary",
+    props: ['numOfReviews', 'rating']
+}
+</script>
+
+<style>
+
+</style>
