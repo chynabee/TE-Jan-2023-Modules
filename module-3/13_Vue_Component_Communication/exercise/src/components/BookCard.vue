789bf19b5558c0295ed6aa091c55938cb6c8dbff
Chyna Broyles <chynabroyles@gmail.com>
2023-04-05 16:10:35 -0400

    Vue Component

--- module-3/13_Vue_Component_Communication/exercise/src/components/BookCard.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/BookCard.vue
@@ -1,47 +1,55 @@
 <template>
   <div class="card">
+    <h2 class="book-title">{{ book.title }}</h2>
+    <h3 class="book-author">{{ book.author }}</h3>
+    <img class="book-image" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'"/>
+
+    <button class="mark-read" 
+      v-if="book.read == false" 
+      v-on:click="onReadUnreadChange"> 
+      {{ book.read === true ? "Mark Unread" : "Mark Read" }} 
+    </button>
+
+    <button
+      class="mark-unread"
+      v-if="book.read == true"
+      v-on:click="onReadUnreadChange">
+      {{ book.read === true ? "Mark Unread" : "Mark Read" }}
+    </button>
 
   </div>
 </template>
 
 <script>
 export default {
-    name: 'book-card'
-}
+  name: "book-card",
+  props: ["book"],
+  methods: {
+    onReadUnreadChange() {
+      this.$store.commit("TOGGLE_READ_UNREAD", this.book);
+    },
+  },
+};
 </script>
 
 <style>
 .card {
-    border: 2px solid black;
-    border-radius: 10px;
-    width: 250px;
-    height: 550px;
-    margin: 20px;
-    position: relative;
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 250px;
+  height: 550px;
+  margin: 20px;
 }
-
 .card.read {
-    background-color: lightgray;
+  background-color: lightgray;
 }
-
 .card .book-title {
-    font-size: 1.5rem;
+  font-size: 1.5rem;
 }
-
 .card .book-author {
-    font-size: 1rem;
+  font-size: 1rem;
 }
-
 .book-image {
     width: 80%;
-}
-
-.mark-read, .mark-unread {
-    display: block;
-    position: absolute;
-    bottom: 40px;
-    width: 80%;
-    left: 10%;
-    font-size: 1rem;
 }
 </style>
--- module-3/13_Vue_Component_Communication/exercise/src/components/ReadingList.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/ReadingList.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="book-container">
+    <book-card
+    v-for="bookItem in $store.state.books"
+    v-bind:key="bookItem.title"
+    v-bind:book="bookItem"
+    v-bind:class="{ 'read' : bookItem.read == true}"
+    />
 
   </div>
 </template>
 
 <script>
-
+import BookCard from './BookCard.vue'
 export default {
+  components: { BookCard },
     name: 'reading-list'
 }
 </script>
 
 <style>
 .book-container {
-    display: flex;
+    display:flex;
     justify-content: space-evenly;
     flex-wrap: wrap;
 }
