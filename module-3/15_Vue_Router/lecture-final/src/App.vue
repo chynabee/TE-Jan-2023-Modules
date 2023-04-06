a5d0365648d4044f98993511423aed93de6e7261
Test Student <christopher@techelevator.com>
2023-04-06 12:01:48 -0400

    lecture and hw

---
+++ module-3/15_Vue_Router/lecture-final/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h2>Top of our page</h2>
+  <router-view />
+  <h3>End of our page</h3>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
